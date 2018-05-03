e28de9f32ca90d6e5daea8149ebbc4a5492a5019
Tom McLaughlin <pyro777@gmail.com>
2018-05-02 18:36:43 -0700

    Cleanup pass

--- sia.py
+++ sia.py
@@ -1,100 +1,108 @@
+import cvxopt
+import cvxopt.modeling
 import numpy as np
 import math
 from scipy.optimize import minimize
-import cvxopt.modeling
+import sys
 
-def context_vector(age, gender):
-	pass
+# Pick the right range to use (Python 3 range = Python 2 xrange)
+range = range if sys.version_info >= (3, 0) else xrange
 
 class Bandit:
+    def __init__(self, regularization_param=0.1, num_items=3, num_positions=2, len_contexts=3):
+        self.K = num_items
+        self.M = num_positions
+        self.S = num_positions
+        self.C = len_contexts
+        self.D = len_contexts + num_positions * 2 + num_items
+        self.m = np.zeros(self.D)
+        self.q = np.full(self.D, regularization_param)
 
-	def __init__(self, regularization_param=0.1, num_items=3, num_positions=2, len_contexts=3):
-		self.K = num_items
-		self.M = num_positions
-		self.S = num_positions
-		self.C = len_contexts
-		self.D = len_contexts + num_positions * 2 + num_items
-		self.lamda = regularization_param
-		self.m = cvxopt.matrix(np.zeros(self.D))
-		self.q = cvxopt.matrix(np.full(self.D, regularization_param))
+    def handle_user_action(self, context, item, pos, reward):
+        # n-hot vector saying which item(s) were picked (currently just one)
+        gamma = np.zeros(self.M)
+        gamma[pos] = 1
 
-	def sample_posterior(self):
-		w = np.zeros(self.D)
-		for j in xrange(self.D):
-			print("q:", self.q[j])
-			w[j] = np.random.normal(self.m[j], 1/math.sqrt(self.q[j]))
-		return w
+        # Two one-hot vectors stuck together, encoding putting "item" at "pos"
+        # TODO(tom): I think this might be wrong, it seems like we should be putting *all* the
+        # items in from the superarm that the user saw
+        phi = np.zeros(self.K + self.M)
+        phi[item] = 1
+        phi[pos + self.K] = 1
 
-	def expected_values(self, context):
-		e = np.random.rand(self.K,self.M)
-		return e
+        # Context vector
+        psi = np.zeros(self.C)
+        psi[0] = context["age"]
+        psi[1] = 1 if context["gender"] == "male" else 0
+        psi[2] = 1 if context["gender"] == "female" else 0
 
-	def optimizer(self, expected_values):
-		f = []
-		for j in range(self.M):
-			f.append(cvxopt.modeling.variable(self.K, "position_%d" % j))
-		get_column = lambda i: cvxopt.matrix(expected_values[:, i])
-		constraints = []
-		for vec in f:
-			constraints.append(vec >= 0)
-			constraints.append(vec <= 1)
-			constraints.append(sum(vec) <= 1)
-		constraints.append(sum(sum(x) for x in f) == self.S)
-		for k in xrange(self.K):
-			constraints.append(sum([f[m][k] for m in xrange(len(f))]) <= 1)
-		op = cvxopt.modeling.op(sum(cvxopt.modeling.dot(f[i], get_column(i)) for i in xrange(len(f))), constraints)
-	 	op.solve()
-	 	return f
+        (new_m, new_q) = self.update_posterior(np.concatenate((phi, psi, gamma)), reward, self.m, self.q)
 
+        # Update the m and q vectors
+        self.m = new_m
+        self.q = new_q
 
-	def update_posterior(self, event, reward):
-		print(len(event))
-		
-		def objective(w):
-			expInside = -1 * reward * np.dot(w, event)
-			term1 = -.5 * sum(self.q[i] * (w[i]-self.m[i]) * (w[i]-self.m[i]) for i in range(self.D))
-			term2 = -math.log(1+math.exp(expInside))
-			result = -(term1 + term2)
-			return result
+    def get_items(self, context):
+        w = self.sample_posterior(self.m, self.q)
+        # select actions
+        e = self.expected_values(self.K, self.M, context)
+        f = self.compute_superarm(e, self.S)
+        return f
 
-		x0 = np.zeros(len(event))
-		res = minimize(objective, x0, method='nelder-mead', options={'xtol': 1e-8, 'disp': True})
+    @staticmethod
+    def sample_posterior(m, q):
+        """Return a sample of the parameters (a vector of independent Gaussians with means m and precisions q)"""
+        return [np.random.normal(m[j], 1/math.sqrt(q[j])) for j in range(len(m))]
 
-		print("res: ", res)
+    @staticmethod
+    def expected_values(K, M, context):
+        e = np.random.rand(K, M)
+        # TODO(tom): we still need to fill this in. The expected values need to be
+        # \sigma(\Psi . w), where capital Psi is the big concatenated vector we make in
+        # handle_user_action. We should probably extract a helper method to generate the big vector
+        # given a set of actions and context
+        return e
 
-		sigmoid = lambda x: 1 / (1 + math.exp(-x))
+    @staticmethod
+    def compute_superarm(expected_values, S):
+        """Given a matrix of expected rewards, solve the LP to get the best super-arm with S items"""
+        (K, M) = expected_values.shape
 
-		for j in range(self.D):
-			self.m[j] = res.x[j]
-			cw_std = sigmoid(np.dot(res.x, event))
-			print("original: ", self.q[j])
-			print("cw_std: ", cw_std)
-			self.q[j] = self.q[j] + cw_std * (1-cw_std)*event[j]**2
-			print("updated: ", self.q[j])
-			if (self.q[j] < 0): raise Exception("AAAH")
+        # Variables
+        f = [cvxopt.modeling.variable(K, "position_%d" % j) for j in range(M)]
 
-	def get_items(self, context):
-		w = self.sample_posterior()
-		# select actions
-		e = self.expected_values(context)
-		f = self.optimizer(e)
-		return f
+        # Objective
+        objective = sum(cvxopt.modeling.dot(f[i], cvxopt.matrix(expected_values[:, i])) for i in range(len(f)))
 
-	def handle_user_action(self, context, item, pos, reward):
-		gamma = np.zeros(self.M)
-		gamma[pos] = 1
-		psi = np.zeros(self.K + self.M)
-		psi[item] = 1
-		psi[pos + self.K] = 1
+        # Constraints
+        constraints = []
+        for vec in f: constraints.append(vec >= 0)
+        for vec in f: constraints.append(vec <= 1)
+        constraints.append(sum(sum(x) for x in f) == S)
+        for vec in f: constraints.append(sum(vec) <= 1) # Column sums don't exceed 1
+        for k in range(K): constraints.append(sum([f[m][k] for m in range(len(f))]) <= 1) # Row sums don't exceed 1
 
-		phi = np.zeros(self.C)
-		phi[0] = context["age"]
-		phi[1] = 1 if context["gender"] == "male" else 0
-		phi[2] = 1 if context["gender"] == "female" else 0
-		self.update_posterior(np.concatenate((phi,psi,gamma), axis=0), reward)
-		# m = np.zeros(self.M)
-		# Get position item indeces
-		# for j in xrange
+        cvxopt.modeling.op(objective, constraints).solve()
+
+        return f
+
+    @staticmethod
+    def update_posterior(event, reward, m, q):
+        """Run a solver to find an updated m and q given the event and reward."""
+        def objective(w):
+            term1 = -0.5 * sum(q[i] * (w[i]-m[i]) * (w[i]-m[i]) for i in range(len(event)))
+            term2 = -math.log(1+math.exp(-1 * reward * np.dot(w, event)))
+            result = -(term1 + term2)
+            return result
+
+        x0 = np.zeros(len(event))
+        res = minimize(objective, x0, method='nelder-mead', options={'xtol': 1e-6, 'disp': False})
+
+        prob = 1 / (1 + math.exp(-np.dot(res.x, event)))
+        new_q = q + prob * (1 - prob) * (event**2)
+
+        return (res.x, new_q)
+
 
 b = Bandit()
 ctx = np.array([33,0])
